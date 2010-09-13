15959f982020a30850644491733f1c87d0ddc29e
parrot_studio <parrot@users.sourceforge.jp>
2010-09-13 20:06:55 +0900

    first release

---
+++ shoborn.js
@@ -0,0 +1,146 @@
+var SHOBORN = function(){
+  var that = {};
+
+  var search_param = {
+    q : "\uff65\u03c9\uff65",
+    rpp : 100,
+    page : 1,
+    locale : "ja",
+    show_user : true
+  };
+
+  var result = [];
+  var index = 0;
+  var page_index = 1;
+  var view_level = 1;
+
+  var level3 = /[\(（]\s*[\´\']\s*[･・]\s*ω\s*[･・]\s*[\`｀]\s*[\)）]/;
+  var level2a = /[\(（]\s*[\´\']\s*[･・]\s*ω\s*[･・]\s*[\`｀]?\s*[\)）]?/;
+  var level2b = /[\(（]?\s*[\´\']?\s*[･・]\s*ω\s*[･・]\s*[\`｀]\s*[\)）]/;
+  var level1 = /[\(（]?\s*.?\s*[･・]\s*ω\s*[･・]\s*.?\s*[\)）]?/;
+
+  var level_judge = function(t){
+    if (t === ""){
+      return 0;
+    }
+
+    if (level3.test(t)){
+      return 3;
+    } else if(level2a.test(t)){
+      return 2;
+    } else if(level2b.test(t)){
+      return 2;
+    } else if(level1.test(t)){
+      return 1;
+    }
+
+    return 0;
+  }
+
+  var tweet = function(t){
+    var that = {};
+    var raw = t || {};
+
+    var get = function(name){
+      return raw[name] ? raw[name] : "";
+    }
+
+    that.id = function(){
+      return get("id");
+    }
+
+    that.user = function(){
+      return get("from_user");
+    }
+
+    that.text = function(){
+      return get("text");
+    }
+
+    that.img_url = function(){
+      return get("profile_image_url");
+    }
+
+    that.date = function(){
+      var d = get("created_at");
+      if (d === ""){
+        return ""
+      }
+      return new Date(d).toLocaleString();
+    }
+
+    that.level = function(){
+      return level_judge(get("text"));
+    }
+
+    return that;
+  }
+
+  var parse_tweet = function(data){
+    $.each(data, function(i, d){
+      result.push(tweet(d));
+    });
+  }
+
+  that.search = function(func){
+    search_param.page = page_index;
+    $.getJSON("http://search.twitter.com/search.json?callback=?", search_param, function(data){
+      parse_tweet(data.results);
+      page_index += 1
+      func();
+    });
+  }
+
+  that.shoborn_test = function(name, func){
+    search_param.page = page_index;
+    $.getJSON("./" + name, {}, function(data){
+      parse_tweet(data.results);
+      page_index += 1
+      func();
+    });
+  }
+
+  that.results = function(){
+    var ret = [];
+    $.each(result, function(i, r){
+      if (r.level() >= view_level){
+        ret.push(r);
+      }
+    });
+    return ret;
+  }
+
+  that.set_level = function(l){
+    view_level = l;
+  }
+
+  that.result_first = function(){
+    index = 0;
+  }
+
+  that.result_next = function(){
+    var ret = null;
+
+    while(index < result.length){
+      var r = result[index];
+      index += 1;
+      if (r.level() >= view_level){
+        ret = r
+        break;
+      }
+    }
+
+    return ret;
+  }
+
+  that.result_clear = function(){
+    result = [];
+    index = 0;
+  }
+
+  that.view_level = function(){
+    return view_level;
+  }
+
+  return that;
+}();
---
+++ view.js
@@ -0,0 +1,113 @@
+var VIEW = function(){
+  var that = {};
+  var timer = null;
+  var roop_time = 1200;
+  var fade_time = 500;
+  var user_parser = /@([a-zA-Z0-9_]+)/g;
+
+  var stop_timer = function(){
+    if (timer !== null){
+      clearTimeout(timer);
+    }
+    timer = null;
+  }
+
+  var view = function(){
+    var t = SHOBORN.result_next();
+    if (t === null){
+      stop_timer();
+      $("#more").fadeIn(fade_time);
+      return;
+    }
+
+    $("#list").append(tweet_block(t));
+    var target = $("#tw" + t.id());
+    target.addClass(function(){
+      return "lv" + t.level();
+    });
+    target.fadeIn(500);
+
+    timer = setTimeout(function() {
+      view();
+    }, roop_time);
+  }
+
+  var tweet_block = function(t){
+    var b = "";
+    b += "<div class='tweet_block' id='tw" + t.id() + "'>";
+
+    b += "<div class='tweet_left'>";
+    b += "<div class='tweet_lv'>Lv:" + t.level() + "</div>";
+    b += "<div class='tweet_img'>";
+    b += "<img class='tweet_icon' src='" + t.img_url() + "'/>";
+    b += "</div>";
+    b += "</div>";
+
+    b += "<div class='tweet_body'>";
+    b += "<div class='tweet_user'> From : " + user_link(t.user()) + "</div>";
+    b += "<div class='tweet_text'>" + replace_user_link(t.text()) + "</div>";
+    b += "<div class='tweet_date'>";
+    b += "<a href='http://twitter.com/" + t.user + "/status/" + t.id() + "'>";
+    b += t.date() + "</a>"
+    b  += "</div>";
+    b += "</div>";
+
+    b += "</div>";
+
+    return b;
+  }
+
+  var user_link = function(u, h){
+    h = h || "";
+    return "<a href='http://twitter.com/" + u + "' target='_blank'>" + h + u + "</a>";
+  }
+
+  var replace_user_link = function(t){
+    return t.replace(user_parser, function(c, name){
+      return user_link(name, '@');
+    });
+  }
+
+  that.clear = function(){
+    stop_timer();
+    $("#list").empty();
+    $("#more").hide();
+  }
+
+  that.change_level = function(lv){
+    SHOBORN.set_level(lv);
+    SHOBORN.result_first();
+    that.clear();
+    view();
+  }
+
+  that.reset = function(){
+    that.clear();
+    SHOBORN.result_clear();
+  }
+
+  that.search = function(){
+    $("#more").hide();
+    SHOBORN.search(function(){
+      view();
+    });
+  }
+
+  that.search_test = function(){
+    $("#more").hide();
+    SHOBORN.shoborn_test('data1.json', function(){
+      view();
+    });
+  }
+
+  that.view_level = function(){
+    return SHOBORN.view_level();
+  }
+
+  return that;
+}();
+
+
+
+
+
